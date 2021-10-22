5fd787780ad8aa955b6d7601b025731a608d51c9
Çağlar Demir <cagdem2@gmail.com>
2021-10-23 02:32:13 +0300

    add user and response models

---
+++ Ecommerce/Ecommerce.Application/Dtos/LoginUserDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.Dtos
+{
+    public class LoginUserDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Password { get; set; }
+
+    }
+}
---
+++ Ecommerce/Ecommerce.Application/Dtos/RegisterUserDto.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Application.Dtos
+{
+    public class RegisterUserDto
+    {   [Required]
+        [StringLength(56,MinimumLength = 2)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(56, MinimumLength = 2)]
+        public string LastName { get; set; }
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Password { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string ConfirmPassword { get; set; }
+    }
+}
